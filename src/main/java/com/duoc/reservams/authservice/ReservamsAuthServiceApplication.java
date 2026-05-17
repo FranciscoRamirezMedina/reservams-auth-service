349c3acdecd6435f51c0618fd3c67cd909e0ddd3
Francisco Ramírez <TUEMAIL@gmail.com>
2026-05-17 02:53:39 -0400

    chore: create auth service base project

---
+++ src/main/java/com/duoc/reservams/authservice/ReservamsAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.reservams.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservamsAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservamsAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duoc/reservams/authservice/ReservamsAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.reservams.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservamsAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
